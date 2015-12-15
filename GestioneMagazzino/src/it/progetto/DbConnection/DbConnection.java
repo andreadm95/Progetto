0917d6a5860426c076fd8323276f5eeacde229f5
Andrea <Andrea@Pc-Andrea>
2015-12-15 17:36:47 +0100

    Risistemato il DB!

--- GestioneMagazzino/src/it/progetto/DbConnection/DbConnection.java
+++ GestioneMagazzino/src/it/progetto/DbConnection/DbConnection.java
@@ -17,7 +17,7 @@ public class DbConnection {
 	   if(instance == null)
 		   instance = new DbConnection();
 	   if(connesso != true)
-			connetti("mydb", "root", "root");
+			connetti("gestionemagazzino", "root", "root");
 	   return instance;
    }
    
