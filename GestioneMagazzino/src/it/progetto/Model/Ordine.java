9529b10a8c59d40565568285584f99f64f65a44a
Andrea <Andrea@Andrea-PC>
2015-12-23 21:02:21 +0100

    Creato costruttore di ordine

--- GestioneMagazzino/src/it/progetto/Model/Ordine.java
+++ GestioneMagazzino/src/it/progetto/Model/Ordine.java
@@ -4,27 +4,52 @@ import java.util.ArrayList;
 
 public class Ordine {
 
+	  private int idOrdine;
 	  private ArrayList<Prodotto> ListaProdottiAcquisto;
-      public Progetto ProgettoDiRiferimento;
-	  public float SpesaOrdine;
-	  public boolean MagazzinoVicino;
-  
-	  public ArrayList<Prodotto> aggiungiProdottoACarrello(Prodotto a)
+      private int ProgettoDiRiferimento;
+	  private float SpesaOrdine;
+	  private int CodiceMagazzino;
+	  private int DipendenteDiRiferimento;
+
+	public ArrayList<Prodotto> aggiungiProdottoACarrello(Prodotto a)
 	    {
 	    	ListaProdottiAcquisto.add(a);
 	    	return ListaProdottiAcquisto;
 	    }
+	
+	public Ordine(int id, int codmagazzino, int cod_dipendente, int codprogetto){
+		idOrdine=id;
+		CodiceMagazzino=codmagazzino;
+		DipendenteDiRiferimento=cod_dipendente;
+		ProgettoDiRiferimento=codprogetto;
+	}
 	  
+	 public int getIdOrdine() {
+			return idOrdine;
+		}
+
+		public void setIdOrdine(int idOrdine) {
+			this.idOrdine = idOrdine;
+		}
+
+		public int getDipendenteDiRiferimento() {
+			return DipendenteDiRiferimento;
+		}
+
+		public void setDipendenteDiRiferimento(int dipendenteDiRiferimento) {
+			DipendenteDiRiferimento = dipendenteDiRiferimento;
+		}
+	
 	public ArrayList<Prodotto> getListaProdottiAcquisto() {
 		return ListaProdottiAcquisto;
 	}
 	public void setListaProdottiAcquisto(ArrayList<Prodotto> listaProdottiAcquisto) {
 		ListaProdottiAcquisto = listaProdottiAcquisto;
 	}
-	public Progetto getProgettoDiRiferimento() {
+	public int getProgettoDiRiferimento() {
 		return ProgettoDiRiferimento;
 	}
-	public void setProgettoDiRiferimento(Progetto progettoDiRiferimento) {
+	public void setProgettoDiRiferimento(int progettoDiRiferimento) {
 		ProgettoDiRiferimento = progettoDiRiferimento;
 	}
 	public float getSpesaOrdine() {
@@ -33,11 +58,11 @@ public class Ordine {
 	public void setSpesaOrdine(float spesaOrdine) {
 		SpesaOrdine = spesaOrdine;
 	}
-	public boolean isMagazzinoVicino() {
-		return MagazzinoVicino;
+	public int getCodiceMagazzino() {
+		return CodiceMagazzino;
 	}
-	public void setMagazzinoVicino(boolean magazzinoVicino) {
-		MagazzinoVicino = magazzinoVicino;
+	public void setCodiceMagazzino(int CodMagazzino) {
+		CodiceMagazzino = CodMagazzino;
 	}
 	
 	
