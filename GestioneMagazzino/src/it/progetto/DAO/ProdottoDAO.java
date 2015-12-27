a47ee708252a4ff2b516d047bac6f5c88c68fd2e
Francesco <Francesco@192.168.1.108>
2015-12-27 17:00:38 +0100

    Creazione classe ProdottoDAO con metodo per la modifica della quantità di prodotti

---
+++ GestioneMagazzino/src/it/progetto/DAO/ProdottoDAO.java
@@ -0,0 +1,28 @@
+package it.progetto.DAO;
+
+import it.progetto.DbConnection.DbConnection;
+import java.util.ArrayList;
+import java.util.Vector;
+import it.progetto.Model.Prodotto;
+
+public class ProdottoDAO {
+	
+	private static ProdottoDAO instance;
+	
+	public static ProdottoDAO getInstance()
+	{
+		if(instance==null)
+		{
+			instance = new ProdottoDAO();
+		}
+		return instance;		
+	}
+	
+	public void RiduciQuantit‡Prodotto(ArrayList<Prodotto> lista, String nome, int sede)
+		{
+			for(int i=0; i< lista.size();i++)
+			{
+				boolean result=DbConnection.getInstance().eseguiAggiornamento("update Prodotto,Appartiene set Prodotto.Disponibilit‡=Prodotto.Disponibilit‡-1 where Prodotto.Nome="+lista.get(i).getNome()+"and Appartiene.CodMagazzino="+sede+"and Prodotto.IdProdotto=Appartiene.CodProdotto");
+			}
+		}
+}
