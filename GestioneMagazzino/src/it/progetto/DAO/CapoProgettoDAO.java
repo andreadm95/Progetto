dbc71fd69ff77c1ccbf50280330886a780e23dc9
Andrea <Andrea@Andrea-PC>
2016-01-07 18:55:22 +0100

    Fine riordino spese capoprogetto

--- GestioneMagazzino/src/it/progetto/DAO/CapoProgettoDAO.java
+++ GestioneMagazzino/src/it/progetto/DAO/CapoProgettoDAO.java
@@ -29,13 +29,37 @@ private static CapoProgettoDAO instance;
 			}
 	}
 	
+	public boolean PiùDiDueProgetti(int idCapo){
+		Vector<String[]> result=DbConnection.getInstance().eseguiQuery("SELECT * FROM progetto WHERE CodCapoProgetto=\""+idCapo+"\"");
+		if(result.size()<2){
+			return false;
+		}
+		else{return true;}
+	}
+	
 	public Vector<String[]> OrdinaSpeseProgetto(int idCapo){
-		Vector<String[]> result=DbConnection.getInstance().eseguiQuery("SELECT * FROM progetto WHERE CodCapoProgetto=\""+idCapo+"\" ORDER BY idProgetto;");
+		Vector<String[]> result=DbConnection.getInstance().eseguiQuery("SELECT * FROM progetto WHERE CodCapoProgetto=\""+idCapo+"\" ORDER BY idProgetto");
 		if(result.size()!=0){
 			return result;}
 		else{
 		return null;
 			}
 		
+	}
+	
+	public Vector<String[]> OrdinaSpeseDipendente(int idCapo){
+		Vector<String[]> ricercaprogetti=DbConnection.getInstance().eseguiQuery("SELECT * FROM progetto WHERE CodCapoProgetto=\""+idCapo+"\"");
+		if(ricercaprogetti.size()!=0){
+			Vector<String[]> result= new Vector<String[]>();
+			for(int i=0;i<ricercaprogetti.size();i++){
+				Vector<String[]> ricercadip=DbConnection.getInstance().eseguiQuery("SELECT Dipendente.* FROM Dipendente,lavorasu WHERE CodProgetto=\""+ricercaprogetti.get(i)[0]+"\"");
+				for(int j=0; j<ricercadip.size();j++){
+				result.addElement(ricercadip.get(j));}
+				}
+			return result;
+			}
+		else{
+		return null;
+			}
 	}
 }
